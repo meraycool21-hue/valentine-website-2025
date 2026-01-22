b754a500f7c9db802c6006445f65dcfa9f624fd1
meraycool21-hue <meraycool21@gmail.com>
2026-01-22 16:39:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss! Ilove you and im grateful to have you in my life❤️",
+        message: "Now come get your gift, a big warm hug and a huge kiss! Ilove you and im grateful to have you in my life❤️❤️❤️",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
